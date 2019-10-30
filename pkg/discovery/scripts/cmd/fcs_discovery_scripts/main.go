b43426b75eab70881a56df2dbc3b4e70525b9f76
mbana <m@bana.io>
2019-10-30 15:13:33 +0000

    Generate all non-mandatory fields recursively: Run `pkg/discovery/scripts/cmd/fcs_discovery_scripts/main.go --swagger_path 'pkg/schema/spec/v3.1.2/account-info-swagger.flattened.json'`

---
+++ pkg/discovery/scripts/cmd/fcs_discovery_scripts/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"bitbucket.org/openbankingteam/conformance-suite/pkg/discovery/scripts"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	logger  = logrus.StandardLogger()
+	rootCmd = &cobra.Command{
+		Use:   "fcs_discovery_scripts",
+		Short: "Functional Conformance Suite Server -  Discovery Scripts",
+		Long: `Generates all the non-manadatory fields for each endpoint, e.g.,
+
+$ go run pkg/discovery/scripts/cmd/fcs_discovery_scripts/main.go --swagger_path 'pkg/schema/spec/v3.1.2/account-info-swagger.flattened.json'
+		`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			logger := logger.WithFields(logrus.Fields{
+				"app": "fcs_discovery_scripts",
+			})
+
+			swaggerPath := viper.GetString("swagger_path")
+			logger.WithFields(logrus.Fields{
+				"swagger_path": swaggerPath,
+			}).Infof("Parsing started ...")
+
+			nonManadatoryFields, err := scripts.ParseSchema(swaggerPath, logger.Logger)
+			if err != nil {
+				return err
+			}
+
+			logger.WithFields(logrus.Fields{
+				"nonManadatoryFields": fmt.Sprintf("%+v", nonManadatoryFields),
+			}).Infof("Parsing finished ...")
+			return nil
+		},
+	}
+)
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprint(os.Stderr, err)
+		fmt.Fprint(os.Stderr, "\n")
+		os.Exit(1)
+	}
+}
+
+func init() {
+	persistentFlags := rootCmd.PersistentFlags()
+
+	persistentFlags.String("log_level", "INFO", "Log level")
+	persistentFlags.StringP("swagger_path", "s", "", "Swagger file path, e.g., 'pkg/schema/spec/v3.1.2/account-info-swagger.flattened.json'")
+
+	if err := viper.BindPFlags(rootCmd.PersistentFlags()); err != nil {
+		fmt.Fprint(os.Stderr, err)
+		fmt.Fprint(os.Stderr, "\n")
+		os.Exit(1)
+	}
+
+	viper.SetEnvPrefix("")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+
+	cobra.MarkFlagRequired(persistentFlags, "swagger_path")
+	cobra.OnInitialize(onInitialize)
+}
+
+func onInitialize() {
+	logger.SetNoLock()
+	logger.SetFormatter(&logrus.TextFormatter{
+		DisableColors:    false,
+		ForceColors:      true,
+		FullTimestamp:    false,
+		DisableTimestamp: true,
+	})
+	level, err := logrus.ParseLevel(viper.GetString("log_level"))
+	if err != nil {
+		printCommandFlags()
+		fmt.Fprint(os.Stderr, err)
+		fmt.Fprint(os.Stderr, "\n")
+		os.Exit(1)
+	}
+	logger.SetLevel(level)
+
+	printCommandFlags()
+}
+
+func printCommandFlags() {
+	rootCmd.PersistentFlags().PrintDefaults()
+
+	logger.WithFields(logrus.Fields{
+		"swagger_path": viper.GetString("swagger_path"),
+		"log_level":    viper.GetString("log_level"),
+	}).Info("configuration flags")
+}
---
+++ pkg/discovery/scripts/parser.go
@@ -0,0 +1,188 @@
+package scripts
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/go-openapi/loads"
+	"github.com/go-openapi/spec"
+	"github.com/sirupsen/logrus"
+)
+
+// NonManadatoryFields -
+type NonManadatoryFields struct {
+	SwaggerPath string
+}
+
+// ParseSchema -
+func ParseSchema(swaggerPath string, logger *logrus.Logger) (*NonManadatoryFields, error) {
+	// doc, err := loads.Spec("docs/generator/v2.0.0/account-info-swagger.json")
+	doc, err := loads.Spec(swaggerPath)
+	if err != nil {
+		logger.WithFields(logrus.Fields{
+			"err":         err,
+			"swaggerPath": swaggerPath,
+		}).Fatal("ParseSchema: loads.Spec(swaggerPath)")
+		return nil, err
+	}
+
+	expanded, err := doc.Expanded(nil)
+	if err != nil {
+		logger.WithFields(logrus.Fields{
+			"err": err,
+			"doc": doc,
+		}).Fatal("ParseSchema: doc.Expanded(nil)")
+		return nil, err
+	}
+
+	nonManadatoryFields := &NonManadatoryFields{
+		SwaggerPath: swaggerPath,
+	}
+	spec := expanded.Spec()
+	// data, err := json.Marshal(spec.Definitions["CurrencyAndAmount"])
+	// logger.Infof("Definitions=", string(data))
+
+	// testDefinition := spec.Definitions["CurrencyAndAmount"]
+	// logger.Infof(
+	// 	"spec.Definitions[\"CurrencyAndAmount\"].SchemaProps.Required=%#v",
+	// 	testDefinition.SchemaProps.Required,
+	// )par
+
+	for path, props := range spec.Paths.Paths {
+		// if path != `/accounts/{AccountId}/transactions` {
+		// 	continue
+		// }
+
+		if props.Delete != nil {
+			logger.Infof("Path=%s Delete\n", path)
+			PrintResponses(props.Delete, logger)
+		}
+		if props.Get != nil {
+			fmt.Printf("Path=%s Get\n", path)
+			// data, err := json.Marshal(props.Get)
+			// if err != nil {
+			// 	log.Fatal(err)
+			// }
+			// logger.Infof("spec.Paths.Paths=", string(data))
+			PrintResponses(props.Get, logger)
+		}
+		if props.Head != nil {
+			logger.Infof("Path=%s Head\n", path)
+			PrintResponses(props.Head, logger)
+		}
+		if props.Options != nil {
+			logger.Infof("Path=%s Options\n", path)
+			PrintResponses(props.Options, logger)
+		}
+		if props.Patch != nil {
+			logger.Infof("Path=%s Patch\n", path)
+			PrintResponses(props.Patch, logger)
+		}
+		if props.Post != nil {
+			logger.Infof("Path=%s Post\n", path)
+			// data, err := json.Marshal(props.Post)
+			// if err != nil {
+			// 	log.Fatal(err)
+			// }
+			// fmt.Println("spec.Paths.Paths=", string(data))
+			PrintResponses(props.Post, logger)
+		}
+		if props.Put != nil {
+			logger.Infof("Path=%s Put\n", path)
+			PrintResponses(props.Put, logger)
+		}
+	}
+
+	return nonManadatoryFields, nil
+}
+
+// PrintResponses -
+func PrintResponses(operation *spec.Operation, logger *logrus.Logger) {
+	// logger.Infof("Default=%#v\n", operation.Responses.Default)
+
+	for statusCode, response := range operation.Responses.StatusCodeResponses {
+		// if statusCode != 201 {
+		// 	continue
+		// }
+
+		// logger.Infof("statusCode=%d\n", statusCode)
+		// logger.Infof("Required=%#v\n", response.Schema.Required)
+		// .ResponseProps.Schema.Required
+		// fmt.Println()
+
+		if response.ResponseProps.Schema == nil {
+			// logger.Infof("statusCode=%d, response.ResponseProps.Schema=nil\n", statusCode)
+			continue
+		}
+
+		optionalProperties := []string{}
+		for propName, prop := range response.ResponseProps.Schema.Properties {
+			opts := GetOptionalProperties(propName, prop, "\t", []string{})
+			optionalProperties = append(optionalProperties, opts...)
+		}
+
+		logger.Infof("\tstatusCode=%d\n", statusCode)
+		for _, optionalProperty := range optionalProperties {
+			logger.Infof("\t%#v\n", optionalProperty)
+		}
+		fmt.Println()
+	}
+}
+
+// GetOptionalProperties -
+func GetOptionalProperties(propName string, prop spec.Schema, indent string, path []string) []string {
+	// logger.Infof("%sName=%#v\n", indent, propName)
+	// logger.Infof("%sDescription=%#v\n", indent, prop.Description)
+	// logger.Infof("%sID=%#v\n", indent, prop.Schema)
+	// logger.Infof("%spath=%#v\n", indent, path)
+	// logger.Infof("%sType=%#v\n", indent, prop.Type)
+
+	newPath := path
+	if prop.Type.Contains("array") {
+		newPath = append(path, propName+"[*]")
+	} else {
+		newPath = append(path, propName)
+	}
+
+	if (prop.Items == nil || prop.Items.Schema == nil) && len(prop.Properties) <= 0 {
+		return []string{strings.Join(newPath, ".")}
+	}
+
+	optionalProperties := []string{}
+	if prop.Items != nil && prop.Items.Schema != nil {
+		required := prop.Items.Schema.Required
+		if len(required) > 0 {
+			sort.Strings(required)
+		}
+		// logger.Infof("%sProperties=%#v\n", indent, len(prop.Items.Schema.Properties))
+		// logger.Infof("%sRequired=%#v\n", indent, required)
+
+		for propName, prop := range prop.Items.Schema.Properties {
+			pos := sort.SearchStrings(required, propName)
+			found := pos < len(required) && required[pos] == propName
+			if !found {
+				opts := GetOptionalProperties(propName, prop, indent+"\t", newPath)
+				optionalProperties = append(optionalProperties, opts...)
+			}
+		}
+	} else {
+		required := prop.Required
+		if len(required) > 0 {
+			sort.Strings(required)
+		}
+		// logger.Infof("%sProperties=%#v\n", indent, len(prop.Properties))
+		// logger.Infof("%sRequired=%#v\n", indent, required)
+
+		for propName, prop := range prop.Properties {
+			pos := sort.SearchStrings(required, propName)
+			found := pos < len(required) && required[pos] == propName
+			if !found {
+				opts := GetOptionalProperties(propName, prop, indent+"\t", newPath)
+				optionalProperties = append(optionalProperties, opts...)
+			}
+		}
+	}
+
+	return optionalProperties
+}
