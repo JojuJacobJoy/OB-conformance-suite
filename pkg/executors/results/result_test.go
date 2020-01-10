89d6cabbb4acf37f00892f6f90752cd03cca34d1
Julian Coombes <julian.coombes@googlemail.com>
2020-01-10 12:59:01 +0000

    tidy

--- pkg/executors/results/result_test.go
+++ pkg/executors/results/result_test.go
@@ -2,7 +2,10 @@ package results
 
 import (
 	"encoding/json"
+	"fmt"
+	"os"
 
+	"bitbucket.org/openbankingteam/conformance-suite/pkg/netclient"
 	"bitbucket.org/openbankingteam/conformance-suite/pkg/test"
 
 	"errors"
@@ -68,4 +71,51 @@ func TestTestCaseResultJsonMarshal(t *testing.T) {
 	require.NotEmpty(actual)
 
 	require.JSONEq(expected, string(actual))
+}
+
+func TestNewTestCaseResultWithMetrics(t *testing.T) {
+	assert := test.NewAssert(t)
+
+	err := errors.New("some error")
+	result := NewTestCaseResult("123", true, NoMetrics(), []error{err}, "endpoint", "api-name", "api-version", "detailed description", "https://openbanking.org.uk/ref/uri")
+
+	assert.Equal("123", result.Id)
+	assert.True(result.Pass)
+	assert.Equal(NoMetrics(), result.Metrics)
+	assert.Equal(err.Error(), result.Fail[0])
+}
+
+func TestLogToFile(t *testing.T) {
+	f, err := os.OpenFile("testdata/test.log", os.O_CREATE|os.O_WRONLY, 0644)
+	defer f.Close()
+	if err != nil {
+		fmt.Println("Error opening file")
+		t.Fail()
+	}
+
+	netclient.SetDebug(true)
+	netclient.SetLoggerFile(f)
+
+	resp, err := netclient.NewRequest().Get("http://httpbin.org/get")
+
+	if err != nil {
+		fmt.Printf("\nError: %v", err)
+	}
+	fmt.Printf("\nResponse Status Code: %v", resp.StatusCode())
+	fmt.Printf("\nResponse Status: %v", resp.Status())
+	fmt.Printf("\nResponse Body: %v", resp)
+	fmt.Printf("\nResponse Time: %v", resp.Time())
+	fmt.Printf("\nResponse Received At: %v\n", resp.ReceivedAt())
+
+	ti := resp.Request.TraceInfo()
+	fmt.Println("DNSLookup    :", ti.DNSLookup)
+	fmt.Println("ConnTime     :", ti.ConnTime)
+	fmt.Println("TLSHandshake :", ti.TLSHandshake)
+	fmt.Println("ServerTime   :", ti.ServerTime)
+	fmt.Println("ResponseTime :", ti.ResponseTime)
+	fmt.Println("TotalTime    :", ti.TotalTime)
+	fmt.Println("IsConnReused :", ti.IsConnReused)
+	fmt.Println("IsConnWasIdle:", ti.IsConnWasIdle)
+	fmt.Println("ConnIdleTime :", ti.ConnIdleTime)
+
 }
